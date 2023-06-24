d8cd173568b2bc052fae974917e348cd659f2119
oceanny <124882506+oceanny@users.noreply.github.com>
2023-06-24 23:46:56 +0900

    [Bronze II] Title: Hashing, Time: 124 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -3,22 +3,18 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int l = Integer.parseInt(br.readLine());
-		String str = br.readLine();
-		char[] ch = new char[l];
+		String s = br.readLine();
 		long ans = 0;
 		long multi = 1;
-		
-		for (int i = 0; i < l; i++) {
-			ch[i] = str.charAt(i);
-			ans += (ch[i] - 96) * multi;
-			multi *= 31 % 1234567891;
+		for(int i = 0; i < l; i++) {
+			ans += ((s.charAt(i) - 96) * multi);
+			multi = (multi * 31) % 1234567891;
 		}
-		ans = ans % 1234567891;
-		System.out.println(ans);
-
+		System.out.println(ans % 1234567891);
 	}
 }
+
