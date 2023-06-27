ba5126c4371dc157cea30188a00958009e5828cf
oceanny <124882506+oceanny@users.noreply.github.com>
2023-06-27 23:25:54 +0900

    [level 1] Title: 2016년, Time: 0.04 ms, Memory: 70.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12901. 2016년/2016년.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(int a, int b) {
+        String answer = "";
+		String[] day = { "FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU" };
+		int[] month = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+		int sum = 0;
+
+		for (int i = 0; i < a - 1; i++) {
+			sum += month[i];
+		}
+
+		sum += b - 1;
+		answer = day[sum % 7];
+
+		return answer;
+    }
+}
