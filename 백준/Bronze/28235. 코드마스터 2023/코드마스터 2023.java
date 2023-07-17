befcf99ec00af3a36bcb03659229f4077ea461fa
oceanny <124882506+oceanny@users.noreply.github.com>
2023-07-17 19:07:22 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 196 ms, Memory: 17532 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		switch(sc.nextLine()) {
+		case"SONGDO":
+			System.out.println("HIGHSCHOOL");
+			break;
+		case"CODE":
+			System.out.println("MASTER");
+			break;
+		case"2023":
+			System.out.println("0611");
+			break;
+		case"ALGORITHM":
+			System.out.println("CONTEST");
+			break;
+		}
+	}
+}
