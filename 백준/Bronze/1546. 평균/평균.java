e70c13684b80aec8b325a013d509dc23f1448840
oceanny <124882506+oceanny@users.noreply.github.com>
2023-07-18 14:33:08 +0900

    [Bronze I] Title: 평균, Time: 280 ms, Memory: 20252 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,36 +1,18 @@
 import java.util.Scanner;
 
-public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] score = new int[n];
-        float[] nscore = new float[n];
-        float total = 0f;
-        
-        for (int i = 0; i < n; i++) {
-        	score[i] = sc.nextInt();
-        }
-        
-        for (int i = 0; i < n; i++){
-            for (int j = i + 1; j < n; j++){
-                if (score[j] > score[i]){
-                    int temp = score[j];
-                    score[j] = score[i];
-                    score[i] = temp;
-                }
-                
-            }
-        }
-        
-        for (int i = 0; i < n; i++){
-            nscore[i] = score[i] * 100 / (float)score[0];
-        }
-        
-        for (int i = 0; i < n; i++){
-            total = total + nscore[i];
-        }
-        
-        System.out.print(total / n);
-    }
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int max = 0;
+		int sum = 0;
+
+		for (int i = 0; i < n; i++) {
+			int temp = sc.nextInt();
+			if (temp > max) max = temp;
+			sum += temp;
+		}
+		
+		System.out.print(sum * 100.0 / max / n);
+	}
 }
