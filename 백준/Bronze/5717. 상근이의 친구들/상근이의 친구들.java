badb3ae723405fe47482ce1254881fa9400cb085
oceanny <124882506+oceanny@users.noreply.github.com>
2023-09-03 18:57:37 +0900

    [Bronze IV] Title: 상근이의 친구들, Time: 204 ms, Memory: 17488 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		while(A + B != 0) {
+			System.out.println(A + B);
+			A = sc.nextInt();
+			B = sc.nextInt();
+		}
+		sc.close();
+	}
+}
