e6808b769c0d6865cdb9bacd10e6fcd7b772b266
oceanny <124882506+oceanny@users.noreply.github.com>
2023-09-05 10:38:24 +0900

    [Bronze IV] Title: 빵, Time: 128 ms, Memory: 14384 KB -BaekjoonHub

---
+++ 백준/Bronze/25377. 빵/빵.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int min = 1001;
+		
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			if (A > B) continue;
+			if (B < min) min = B;
+		}
+		
+		if (min == 1001) min = -1;
+		System.out.print(min);
+	}
+}
