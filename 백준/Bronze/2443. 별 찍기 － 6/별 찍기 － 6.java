17b73fe74a3fdfdd0fcb904a783a4706956c1d80
oceanny <124882506+oceanny@users.noreply.github.com>
2023-09-11 10:30:00 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 388 ms, Memory: 18632 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		sc.close();
+		
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= i - 1; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 2 * (N - i) + 1; j > 0; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
