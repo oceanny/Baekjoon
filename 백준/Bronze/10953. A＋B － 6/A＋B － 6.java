4a14130e873e09d023e0cafc61381270dc9fcf31
oceanny <124882506+oceanny@users.noreply.github.com>
2023-09-11 10:24:45 +0900

    [Bronze III] Title: A+B - 6, Time: 140 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for (int i = 0; i < T; i++) {
+			String str = br.readLine();
+			int A = Integer.parseInt(str.substring(0, 1));
+			int B = Integer.parseInt(str.substring(2));
+			System.out.println(A + B);
+		}
+	}
+}
