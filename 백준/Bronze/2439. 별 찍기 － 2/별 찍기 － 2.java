cc7edd48da6587ad46d86367e3abe90818d039a7
oceanny <124882506+oceanny@users.noreply.github.com>
2023-09-16 22:40:21 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 428 ms, Memory: 22292 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        for (int i = 1; i <= n; i++){
+            for (int j = n; j > i; j--){
+                System.out.printf(" ");
+            }
+            for (int k = 1; k <= i; k++){
+                System.out.printf("*");
+            }
+            System.out.println();
+        }
+    }
+}
