eb6f44aaaf0466d8a9fd91d23871d44b780d97b6
oceanny <124882506+oceanny@users.noreply.github.com>
2023-12-01 21:52:03 +0900

    [Bronze III] Title: 타슈, Time: 132 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/30018. 타슈/타슈.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] a = new int[N];
+		int[] b = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			a[i] = Integer.parseInt(st.nextToken());
+		}
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			b[i] = Integer.parseInt(st.nextToken());
+		}
+		int diff = 0;
+		for (int i = 0; i < N; i++) {
+			diff += Math.abs(a[i] - b[i]);
+		}
+		System.out.println(diff / 2);
+	}
+}
