a4ee8697d64327273230a6d8974deabd833776b0
oceanny <124882506+oceanny@users.noreply.github.com>
2023-12-12 17:34:21 +0900

    [Bronze III] Title: 얼마?, Time: 308 ms, Memory: 28064 KB -BaekjoonHub

---
+++ 백준/Bronze/9325. 얼마？/얼마？.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for (int i = 0; i < T; i++) {
+			int s = Integer.parseInt(br.readLine());
+			int n = Integer.parseInt(br.readLine());
+			for (int j = 0; j < n; j++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				int q = Integer.parseInt(st.nextToken());
+				int p = Integer.parseInt(st.nextToken());
+				s += q * p;
+			}
+			System.out.println(s);
+		}
+	}
+}
