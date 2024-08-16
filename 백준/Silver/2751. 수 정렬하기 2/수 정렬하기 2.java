596fc2a37b5a705fe2ba44593e283946440cdb35
oceanny <124882506+oceanny@users.noreply.github.com>
2024-08-16 11:07:51 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1344 ms, Memory: 130680 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,27 +1,27 @@
-import java.io.IOException;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main{
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int n = Integer.parseInt(br.readLine());
-		int[] num = new int[n];
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		ArrayList<Integer> list = new ArrayList<>();
 		
-		for (int i = 0; i < n; i++) {
-			num[i] = Integer.parseInt(br.readLine());
+		for (int i = 0; i < N; i++) {
+			list.add(Integer.parseInt(br.readLine()));
 		}
 		
-		Arrays.sort(num);
+		Collections.sort(list);
 		
-		for (int i = 0; i < n; i++) {
-			bw.write(num[i] + "\n");
+		StringBuilder sb = new StringBuilder();
+		for (int val : list) {
+			sb.append(val).append("\n");
 		}
-        
-        bw.flush();
-    }
+		
+		System.out.println(sb);
+	}
 }
