c47f8a034f5a5aaeea4ec8b5e00884e9251f6e02
oceanny <124882506+oceanny@users.noreply.github.com>
2024-08-20 16:07:46 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 100 ms, Memory: 14164 KB -BaekjoonHub

--- 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -1,9 +1,23 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        String str = sc.next();
-        System.out.println(str.length());
-    }
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		int len = 0;
+
+		try {
+			for (int i = 0; i < 100; i++) {
+				str.charAt(i);
+				len++;
+			}
+		} catch (Exception e) {
+			System.out.println(len);
+            return;
+		}
+        System.out.println(len);
+	}
 }
