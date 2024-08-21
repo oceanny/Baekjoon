c64a84dd997332aa9e274a55818041d7ad57c361
oceanny <124882506+oceanny@users.noreply.github.com>
2024-08-21 15:39:11 +0900

    [Bronze I] Title: 2진수 8진수, Time: 296 ms, Memory: 21740 KB -BaekjoonHub

--- 백준/Bronze/1373. 2진수 8진수/2진수 8진수.java
+++ 백준/Bronze/1373. 2진수 8진수/2진수 8진수.java
@@ -1,14 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
 
 public class Main {
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String str = br.readLine();
-		BigInteger N = new BigInteger(str, 2);
-		String answer = N.toString(8);
-		System.out.println(answer);
+		StringBuilder sb = new StringBuilder();
+		
+		for (int i = str.length() - 1; i >= 0; i -= 3) {
+			if (i > 1) {
+				int temp = (str.charAt(i - 2) - '0') * 4 + (str.charAt(i - 1) - '0') * 2 + str.charAt(i) - '0';
+				sb.append(temp);
+			}
+			else if (i == 1) {
+				int temp = (str.charAt(i - 1) - '0') * 2 + str.charAt(i) - '0';
+				sb.append(temp);
+			}
+			else {
+				int temp = str.charAt(i) - '0';
+				sb.append(temp);
+			}
+		}
+		
+		System.out.println(sb.reverse());
 	}
 }
