df1fff0c0c974a8f557ee7d26c9c55578a2d3264
oceanny <124882506+oceanny@users.noreply.github.com>
2025-01-07 16:57:42 +0900

    [Bronze I] Title: FizzBuzz, Time: 104 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Bronze/28702. FizzBuzz/FizzBuzz.java
+++ 백준/Bronze/28702. FizzBuzz/FizzBuzz.java
@@ -3,41 +3,28 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String A = br.readLine();
-		String B = br.readLine();
-		String C = br.readLine();
-		int ans = 0;
-		
-		if (check(A)) {
-			ans = Integer.parseInt(A) + 3;
-		}
-		else if (check(B)) {
-			ans = Integer.parseInt(B) + 2;
-		}
-		else if (check(C)) {
-			ans = Integer.parseInt(C) + 1;
-		}
-		
-		if (ans % 15 == 0) {
-			System.out.println("FizzBuzz");
-		}
-		else if (ans % 3 == 0) {
-			System.out.println("Fizz");
-		}
-		else if (ans % 5 == 0) {
-			System.out.println("Buzz");
-		}
-		else System.out.println(ans);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	private static boolean check(String a) {
-		try {
-			Integer.parseInt(a);
-		} catch (Exception e) {
-			return false;
-		}
-		return true;
-	}
+        String A = br.readLine();
+        String B = br.readLine();
+        String C = br.readLine();
+
+        int ans = check(A) ? Integer.parseInt(A) + 3 : check(B) ? Integer.parseInt(B) + 2 : Integer.parseInt(C) + 1;
+
+        if (ans % 15 == 0) System.out.println("FizzBuzz");
+        else if (ans % 3 == 0) System.out.println("Fizz");
+        else if (ans % 5 == 0) System.out.println("Buzz");
+        else System.out.println(ans);
+    }
+
+    public static boolean check(String str) {
+        try {
+            Integer.parseInt(str);
+        }
+        catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
 }
