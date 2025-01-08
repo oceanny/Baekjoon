8ffac7841e8f15a46cb06d4db1c0fd5e12d019db
oceanny <124882506+oceanny@users.noreply.github.com>
2025-01-08 11:30:53 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1504 ms, Memory: 336484 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -1,25 +1,28 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.Arrays;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Main{
-    public static void main(String[] args) throws IOException{
+public class Main {
+    static int[][] binomial;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		
-		int n = Integer.parseInt(br.readLine());
-		int[] arr = new int[n];
-		for (int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
-		}
-		
-		Arrays.sort(arr);
-		
-		for (int i = 0; i < n; i++) {
-			sb.append(arr[i]).append("\n");
-		}
-		
-		System.out.println(sb);
+        int N = Integer.parseInt(br.readLine());
+        int[] cnt = new int[10001];
+
+        for (int i = 0; i < N; i++) {
+            cnt[Integer.parseInt(br.readLine())]++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i < 10001; i++) {
+            while (cnt[i] > 0) {
+                sb.append(i).append('\n');
+                cnt[i]--;
+            }
+        }
+
+        System.out.println(sb);
     }
 }
