7b4a7ed19f907c650a65130e55bee93559e4a5d8
oceanny <124882506+oceanny@users.noreply.github.com>
2025-07-02 13:22:31 +0900

    [Silver V] Title: 올림픽, Time: 124 ms, Memory: 14696 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.java
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -9,6 +9,7 @@ public class Main {
         int K = Integer.parseInt(st.nextToken());
         int[][] arr = new int[N][4];
         int index = 0;
+        int rank = 1;
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
@@ -16,20 +17,20 @@ public class Main {
             arr[i][1] = Integer.parseInt(st.nextToken());
             arr[i][2] = Integer.parseInt(st.nextToken());
             arr[i][3] = Integer.parseInt(st.nextToken());
-        }
 
-        Arrays.sort(arr, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return o1[1] != o2[1] ? o1[1] - o2[1] : o1[2] != o2[2] ? o1[2] - o2[2] : o1[3] != o2[3] ? o1[3] - o2[3] : o1[0] == K ? 1 : -1;
-            }
-        });
+            if (arr[i][0] == K)
+                index = i;
+        }
 
         for (int i = 0; i < N; i++) {
-            if (arr[i][0] == K) {
-                System.out.println(N - i);
-                return;
-            }
+            if (arr[i][1] > arr[index][1])
+                rank++;
+            else if (arr[i][1] == arr[index][1] && arr[i][2] > arr[index][2])
+                rank++;
+            else if (arr[i][1] == arr[index][1] && arr[i][2] == arr[index][2] && arr[i][3] > arr[index][3])
+                rank++;
         }
+
+        System.out.println(rank);
     }
 }
